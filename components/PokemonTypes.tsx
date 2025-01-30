ab981ac6d4444ea21415304682949c2f792a3b86
Spagestic <steelmax666@gmail.com>
2025-01-30 10:03:45 +0800

    fix: refactor PokemonTypes component to use external typeColors module for improved maintainability

--- components/PokemonTypes.tsx
+++ components/PokemonTypes.tsx
@@ -1,31 +1,9 @@
 import React from "react";
+import { typeColors } from "../lib/typeColors";
 
 interface PokemonType {
   name: string;
 }
-
-const typeColors: { [key: string]: string } = {
-  normal: "bg-[#A8A77A] text-white",
-  fire: "bg-[#EE8130] text-white",
-  water: "bg-[#6390F0] text-white",
-  electric: "bg-[#F7D02C] text-gray-800",
-  grass: "bg-[#7AC74C] text-white",
-  ice: "bg-[#96D9D6] text-gray-800",
-  fighting: "bg-[#C22E28] text-white",
-  poison: "bg-[#A33EA1] text-white",
-  ground: "bg-[#E2BF65] text-gray-800",
-  flying: "bg-[#A98FF3] text-white",
-  psychic: "bg-[#F95587] text-white",
-  bug: "bg-[#A6B91A] text-white",
-  rock: "bg-[#B6A136] text-white",
-  ghost: "bg-[#735797] text-white",
-  dragon: "bg-[#6F35FC] text-white",
-  dark: "bg-[#705746] text-white",
-  steel: "bg-[#B7B7CE] text-gray-800",
-  fairy: "bg-[#D685AD] text-white",
-  stellar: "bg-yellow-300 text-yellow-800", // no official color
-  unknown: "bg-gray-300 text-gray-800", // no official color
-};
 
 async function getPokemonTypes() {
   const res = await fetch("https://pokeapi.co/api/v2/type");
